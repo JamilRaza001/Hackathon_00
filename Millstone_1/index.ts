0e46fe3d6788acfc603f99dd426746b10d803478
Muhammad Jamil Raza Attari <159478196+JamilRaza001@users.noreply.github.com>
2024-11-08 02:31:02 +0500

    Add files via upload

--- Millstone_1/index.ts
+++ Millstone_1/index.ts
@@ -1,11 +1,10 @@
-const Toggle_Btn = document.getElementById('ToggleSkills') as HTMLButtonElement;
-const Skills1 = document.getElementById('Skills') as HTMLElement;
+const Toggle_Btn = document.getElementById("ToggleSkills") as HTMLButtonElement;
+const Skills1 = document.getElementById("Skills") as HTMLElement;
 
-Toggle_Btn.addEventListener('click', ()=>{
-    if(Skills1.style.display === 'none'){
-        Skills1.style.display = 'block';
-    }
-    else{
-        Skills1.style.display = 'none';
-    }
-})
+Toggle_Btn.addEventListener("click", () => {
+  if (Skills1.style.display === "none") {
+    Skills1.style.display = "block";
+  } else {
+    Skills1.style.display = "none";
+  }
+});
